b4676bfba907c51bdbea3ce191a94ea86c981511
Alexandra Ashcraft <lash211@gmail.com>
2023-07-14 15:36:31 -0700

    boilerplate setup

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: { src: './client/index.js' },
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'bundle.js',
+	},
+	mode: process.env.NODE_ENV,
+	module: {
+		rules: [
+			{
+				test: /.(js|jsx)$/,
+				exclude: '/node_modules/',
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env', '@babel/preset-react'],
+				},
+			},
+			{
+				test: /\.(sa|sc|c)ss$/, // styles files
+				exclude: '/node_modules/',
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	devServer: {
+		host: 'localhost',
+		port: 8080,
+		hot: true,
+		historyApiFallback: true,
+		static: {
+			directory: path.resolve(__dirname, 'build'),
+			publicPath: '/build',
+		},
+		proxy: {
+			'/api/*': {
+				target: 'http://localhost:3000/',
+				pathRewrite: { '^/api': '' },
+			},
+		},
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Development',
+			template: 'index.html',
+		}),
+		//new Dotenv(),
+	],
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+};
