f9f3d353decd67f003c3fb6d6248514bb1ba2c4b
Alexandra Ashcraft <lash211@gmail.com>
2023-07-16 15:27:56 -0700

    texting feature working, schedule working

--- server/controllers/textController.js
+++ server/controllers/textController.js
@@ -1,12 +1,9 @@
-// const axios = require('axios');
-// const { text } = require('express');
+const dotEnv = require('dotenv').config();
+const cron = require('node-cron');
 
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(
-	'ACb85b03fe9fcbe43adc47d0c4e875c3aa',
-	'17e2c9a6ab06897a21c9b6ff8a84d3cb'
-);
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 // const apiKey = process.env.TWILIO_API_KEY;
 // const apiSecret = process.env.TWILIO_API_SECRET;
@@ -18,6 +15,44 @@ const client = require('twilio')(
 
 const textController = {};
 
+// textController.sendText = async (req, res, next) => {
+// 	console.log('we are in the textController setupTexts');
+// 	const { username, plant, daysInterval, hour } = req.body;
+// 	if ((!username || !plant || !daysInterval, !hour))
+// 		return next({
+// 			log: 'Error in textControllersetUpText',
+// 			status: 400,
+// 			message: { err: 'Error in request body!' },
+// 		});
+// 	const scheduledMessage = cron.schedule(
+// 		`0 ${hour} */${daysInterval} * *`,
+// 		async () => {
+// 			try {
+// 				const message = await client.messages.create({
+// 					from: '+18449693100',
+// 					messagingServiceSid: 'MG2583b9f6099fae31d8441512947c4e1d',
+// 					body: `Hi, ${username}, Plant Daddy here! It is time to show your ${plant} some love!  Give your ${plant} about 1/4-1/3 of the pot's volume of water and rotate the pot for even sun exposure.  Finish off the job by whispering some sweet nothings into its leaves, and your ${plant} will keep thriving!`,
+// 					to: '+12035544464',
+// 					scheduleType: 'fixed',
+// 					sendAt: new Date(Date.UTC(2023, 0o7, 16, 12, 0o2, 0o0)),
+// 					'content-type': 'application/json',
+// 				});
+// 				console.log(message);
+// 				res.locals.newMessage = message;
+// 				next();
+// 			} catch (error) {
+// 				next({
+// 					log: 'Error in textControllersetUpText',
+// 					status: 400,
+// 					message: { err: 'Error in sending message!', error },
+// 				});
+// 			}
+// 		}
+// 	);
+
+// 	scheduledMessage.start();
+// };
+
 textController.sendText = async (req, res, next) => {
 	console.log('we are in the textController setupTexts');
 	const { username, plant } = req.body;
@@ -27,7 +62,6 @@ textController.sendText = async (req, res, next) => {
 			status: 400,
 			message: { err: 'Error in request body!' },
 		});
-
 	try {
 		const message = await client.messages.create({
 			from: '+18449693100',
@@ -50,20 +84,7 @@ textController.sendText = async (req, res, next) => {
 	}
 };
 
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Find your Account SID at twilio.com/console
-// Provision API Keys at twilio.com/console/runtime/api-keys
-// and set the environment variables. See http://twil.io/secure
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const apiKey = process.env.TWILIO_API_KEY;
-// const apiSecret = process.env.TWILIO_API_SECRET;
-// const client = require('twilio')(apiKey, apiSecret, { accountSid: accountSid });ç
-
 module.exports = textController;
-
-// client.messages
-// .create({from: '+15017122661', body: 'Hi there', to: '+15558675310'})
-// .then(message => console.log(message.sid));
 
 // https://api.twilio.com/2010-04-01/Accounts/{AccountSid}/Messages.json
 
