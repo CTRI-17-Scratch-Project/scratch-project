0794cbd5b229ff8611701438824785f909895b1c
annni11 <ann.j.ni@gmail.com>
2023-07-16 19:03:03 -0400

    annabelle and kevin liveshare - updated popup, db and api working

--- client/components/plantCard.jsx
+++ client/components/plantCard.jsx
@@ -19,6 +19,7 @@ const PlantCard = (props) => {
   //   Blooming_season,
   //   Pruning,
   // } = props.feed;
+ // console.log(props.feed);
   return (
     <div
       className="plant-card"
@@ -27,10 +28,7 @@ const PlantCard = (props) => {
       }}
     >
       <img src={props.img} width={100} height={100}></img>
-      <ul>
-        <li>Name: {props.Common_name}</li>
-        <li>Family: </li>
-      </ul>
+      <p>{props.name}</p>
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -21,36 +21,36 @@ app.use(express.json());
 app.use('*', express.urlencoded({ extended: true }));
 
 //TODO: Write route handler functions
-app.use('/dbAPI', dbRouter);
-app.use('/plantAPI', apiRouter);
+app.use('/api/dbAPI', dbRouter);
+app.use('/api/plantAPI', apiRouter);
 app.use('/textAPI', textRouter);
 
 //handle generic request to serve HTML file
-app.get('/', (req, res) => {
-	return res
-		.status(200)
-		.sendFile(path.resolve(__dirname, '../client/index.html'));
-});
-app.get('/', (req, res) => {});
+// app.get('/', (req, res) => {
+//   return res
+//     .status(200)
+//     .sendFile(path.resolve(__dirname, '../client/index.html'));
+// });
+//app.get('/', (req, res) => {});
 
 //catch-all route for unknown routes
 app.use((req, res) => {
-	res.status(404).send('Oops! Cannot find page!');
+  res.status(404).send('Oops! Cannot find page!');
 });
 
 //global error handler function
 app.use((err, req, res, next) => {
-	const defaultError = {
-		log: 'Express error handler caught unknown middleware error',
-		status: 400,
-		message: { err: 'An error occurred' },
-	};
-	const errorObj = Object.assign({}, defaultError, err);
-	console.log(errorObj.log);
-	return res.status(errorObj.status).json(errorObj.message);
+  const defaultError = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultError, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 //listen on port 3000
 app.listen(PORT, () => {
-	console.log(`Server listening on port: ${PORT}`);
+  console.log(`Server listening on port: ${PORT}`);
 });
