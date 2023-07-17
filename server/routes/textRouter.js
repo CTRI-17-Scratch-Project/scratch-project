d99decab187241cc798193feda6ddb4c711de3bc
Alexandra Ashcraft <lash211@gmail.com>
2023-07-17 13:59:31 -0700

    texts working not posting to DB

--- server/routes/textRouter.js
+++ server/routes/textRouter.js
@@ -5,20 +5,19 @@ const userController = require('../controllers/userController');
 const textRouter = express.Router();
 
 textRouter.post(
-  '/',
-  textController.sendText,
-  userController.updateNotifications,
-  (req, res) => {
-    console.log('I am in the textRouter post handler function');
-    console.log(res.locals.newMessage);
-    return res.status(200).json(res.locals.newMessage);
-  }
+	'/',
+	textController.sendText,
+	userController.updateNotifications,
+	(req, res) => {
+		console.log('I am in the textRouter post handler function');
+		console.log(res.locals.newMessage);
+		return res.status(200).json(res.locals.newMessage);
+	}
 );
 
-
 textRouter.patch('/', userController.updateNotifications, (req, res) => {
-  console.log('I am in the textRouter patch handler function');
-  return res.status(200).json(res.locals.updatedNotifications);
+	console.log('I am in the textRouter patch handler function');
+	return res.status(200).json(res.locals.updatedNotifications);
 });
 
 module.exports = textRouter;
